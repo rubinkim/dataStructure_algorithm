c5467d2715121c819e4ef78d03db8d604ccfecbc
rubinkim <kimyongha2011@gmail.com>
2022-10-09 19:10:17 +0900

    Big problem to encoding

--- chapter_03/insert_functions.py
+++ chapter_03/insert_functions.py
@@ -1,4 +1,4 @@
-# 함수 선언 부분
+# ??? ???? ?占쏙옙?
 def add_data(friend):
     katok.append(None)
     kLen = len(katok)
@@ -6,7 +6,7 @@ def add_data(friend):
     
 def insert_data(position, friend):
     if position < 0 or position > len(katok):
-        print("데이터를 삽입할 범위를 벗어났습니다.")
+        print("you are out of range.")
         return
     
     katok.append(None)
@@ -20,7 +20,7 @@ def insert_data(position, friend):
     
 def delete_data(position):
     if position < 0 or position > len(katok):
-        print("데이터를 삭제할 범위를 벗어났습니다.")
+        print("you are out of range.")
         return
     
     kLen = len(katok)
@@ -33,30 +33,30 @@ def delete_data(position):
     del katok[kLen-1]
     
     
-# 전역 변수 선언 부분
+# ???? ???? ???? ?占쏙옙?
 katok = []
-select = -1           # 1:추가  2:삽입   3:삭제   4:종료
+select = -1           # 1:???  2:????   3:????   4:????
 
 
-# 메인 코드 부분
+# ???? ??? ?占쏙옙?
 if __name__ == "__main__":
     
     while(select != 4):
-        select = int(input("선택하세요(1:추가 2:삽입 3:삭제 4:종료) -->"))
+        select = int(input("Choose(1:add 2:insert 3:delete 4:quit) -->"))
         
         if(select == 1):
-            data = input("추가할 데이터-->")
+            data = input("data to add-->")
             add_data(data)
             print(katok)
             
         elif(select == 2):
-            pos = int(input("삽입할 위치-->"))
-            data = input("추가할 데이터-->")
+            pos = int(input("position to insert-->"))
+            data = input("data to insert-->")
             insert_data(pos, data)
             print(katok)
             
         elif(select == 3):
-            pos = int(input("삭제할 위치-->"))
+            pos = int(input("position to delete-->"))
             delete_data(pos)
             print(katok)
             
@@ -65,5 +65,5 @@ if __name__ == "__main__":
             exit
         
         else:
-            print("1~4 중 하나를 입력하세요.")
+            print("Choose one from 1 to 4.")
             continue
