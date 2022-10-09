53400946ba18fbd3cb6be223ce37026c58a01bb4
rubinkim <kimyongha2011@gmail.com>
2022-10-09 18:41:30 +0900

    More and more on insert_functions.py

--- chapter_03/insert_functions.py
+++ chapter_03/insert_functions.py
@@ -36,3 +36,21 @@ def delete_data(position):
 # 전역 변수 선언 부분
 katok = []
 select = -1           # 1:추가  2:삽입   3:삭제   4:종료
+
+
+# 메인 코드 부분
+if __name__ == "__main__":
+    
+    while(select != 4):
+        select = int(input("선택하세요(1:추가 2:삽입 3:삭제 4:종료) -->"))
+        
+        if(select == 1):
+            data = input("추가할 데이터-->")
+            add_data(data)
+            print(katok)
+            
+        elif(select == 2):
+            pos = int(input("삽입할 위치-->"))
+            data = input("추가할 데이터-->")
+            insert_data(pos, data)
+            print(katok)
