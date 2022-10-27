64bac8c158463b2cd182cd0160875541c668059f
rubinkim <kimyongha2011@gmail.com>
2022-10-27 18:39:18 +0900

    More on SimpleLinkedList.py

--- chapter_04/SimpleLinkedList_01.py
+++ chapter_04/SimpleLinkedList_01.py
@@ -1,4 +1,5 @@
 ## 클래스 및 함수선언 부분
+from email import header
 from os import pread
 from platform import node
 
@@ -15,7 +16,7 @@ def printNodes(start):
         return
     print(current.data, end=' ')
     
-    while(current.link != None):
+    while current.link != None:
         current = current.link
         print(current.data, end=' ')
     print()
@@ -23,5 +24,34 @@ def printNodes(start):
 def insertNode(findData, insertData):
     global memory, head, current, pread
     
-    #
+    # 처음 노드앞에 노드삽입
+    if head.data == findData:
+        node = Node()
+        node.data = insertData
+        node.link = head
+        head = node
+        return
+    
+    # 중간에 있는 노드앞에 노드삽입
+    current = head
+    while current.link != None:
+        pre = current
+        current = current.link
+        if current.data == findData:
+            node = Node()
+            node.data = insertData
+            node.link = current
+            pre.link = node
+            return
+        
+    # 마지막노드뒤에 노드삽입
+    node = Node()
+    node.data = insertData
+    current.link = node
+    
+# 변수선언 부분
+
+        
+        
+    
         
